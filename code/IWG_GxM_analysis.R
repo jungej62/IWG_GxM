01a1303c1014d3c78aa17a3ac5236c61e5b9d5ed
Jake Jungers <jacob.jungers@gmail.com>
2026-08-19 10:31:36 -0500

    First commit to set up the R project for IWG GxM analysis

---
+++ code/IWG_GxM_analysis.R
@@ -0,0 +1,4 @@
+#IWG GxM analysis
+#Objective: Identify IWG genotypes that yield well under variable N conditions
+#Compare NUE among lines
+#Look to see if N rates help specific lines yield well through time
